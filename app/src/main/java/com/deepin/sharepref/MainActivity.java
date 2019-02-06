981eb35b015c42f4099339bad360feeef3b4f27a
deepinbubblegum <5911110222@mutacth.com>
2019-02-06 11:21:38 +0700

    commit

--- app/src/main/java/com/deepin/sharepref/MainActivity.java
+++ app/src/main/java/com/deepin/sharepref/MainActivity.java
@@ -3,12 +3,19 @@ package com.deepin.sharepref;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox chk_savelogin;
+
+    EditText et_username;
+    Button bt_login;
+
     private SharedPreferences mPreferences;
     private String sharedPrefFile =
             "com.example.android.sharepref";
@@ -16,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_username = findViewById(R.id.editText);
+        bt_login = findViewById(R.id.button);
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+                preferencesEditor.putString("USERNAME",et_username.getText().toString());
+                preferencesEditor.apply();
+            }
+        });
 
         chk_savelogin = findViewById(R.id.chk_savelogin);
         chk_savelogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -39,5 +57,7 @@ public class MainActivity extends AppCompatActivity {
         }else {
             chk_savelogin.setChecked(false);
         }
+        String username = mPreferences.getString("USERNAME", null);
+        et_username.setText(username);
     }
 }
